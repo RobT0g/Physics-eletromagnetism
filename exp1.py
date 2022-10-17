965b31d61bccc364072601d15f1017d7d2cd8557
Robson Luan <robsonluan273@hotmail.com>
2022-10-17 02:06:56 -0300

    Pronto

--- exp1.py
+++ exp1.py
@@ -26,6 +26,15 @@ class Eletroscope:
         pygame.draw.line(self.eletro, (184, 115, 51), (100, 10), (100, 300), 10)
         pygame.draw.line(self.eletro, (200, 200, 200), (14, 80), (14, 100), 3)
         pygame.draw.line(self.eletro, (200, 200, 200), (200-14, 80), (200-14, 100), 3)
+        sq = pygame.Surface((18, 18))
+        pygame.draw.rect(sq, (200, 200, 200), pygame.Rect(0, 0, 17, 17))
+        pygame.draw.rect(sq, (0, 0, 0), pygame.Rect(0, 0, 17, 17), 1)
+        self.zero = sq.copy()
+        pygame.draw.circle(self.zero, (0, 0, 0), (9, 9), 5, 2)
+        pygame.draw.line(sq, (0, 0, 0), (3, 8), (14, 8), 2)
+        self.msq = sq.copy()
+        self.psq = sq.copy()
+        pygame.draw.line(self.psq, (0, 0, 0), (8, 3), (8, 14), 2)
     
     def drawSheets(self):
         pos = [
@@ -35,11 +44,22 @@ class Eletroscope:
         pygame.draw.line(self.display, (200, 200, 200), pos[0][0], pos[0][1], 3)
         pygame.draw.line(self.display, (200, 200, 200), pos[1][0], pos[1][1], 4)
         pygame.draw.line(self.display, (200, 200, 200), pos[1][0], pos[1][2], 4)
-    
+
+    def update(self):
+        pos = pygame.mouse.get_pos()
+        if pos[0] > 400 and pos[0] < 418 and pos[1] > 250 and pos[1] < 268 and self.charge < 45:
+            self.charge = self.charge+2.5
+        if pos[0] > 400 and pos[0] < 418 and pos[1] > 270 and pos[1] < 288 and self.charge > 0:
+            self.charge = self.charge-2.5
+        if pos[0] > 400 and pos[0] < 418 and pos[1] > 290 and pos[1] < 308:
+            self.charge = 0
 
     def putOnScreen(self):
         self.display.blit(self.frame, (0, 0))
         self.display.blit(self.eletro, (150, 150))
+        self.display.blit(self.psq, (400, 250))
+        self.display.blit(self.msq, (400, 270))
+        self.display.blit(self.zero, (400, 290))
         self.drawSheets()
         pygame.display.flip()
 
@@ -48,12 +68,8 @@ el = Eletroscope()
 running = True
 while running:
     for e in pygame.event.get():
-        '''if e.type == update:
-            mola.update()
-            mola.putOnScreen()
         if e.type == pygame.MOUSEBUTTONDOWN:
-            mola.refresh()
-        if e.type == pygame.MOUSEBUTTONUP:
-            mola.clicked = False'''
+            el.update()
+            el.putOnScreen()
         if pygame.key.get_pressed()[pygame.K_ESCAPE]:
             running = False
